1574383702b962a3b52d5b96692523ac4ffc21ad
Abdulazeem-Idris <omofolahan.19@gmail.com>
2023-09-09 02:12:50 +0100

    tried another sets of code

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -6,21 +6,43 @@
 
 // export default Grid;
 
+// import React from 'react';
+// import { range } from './utils';
+
+// function Grid({ numRows, numCols }) {
+//   return (
+//     <div className="grid">
+//       {range(1, numRows).map((rowNum) => (
+//         <div className="row" key={rowNum}>
+//           {range(1, numCols).map((colNum) => (
+//             <div className="cell" key={colNum}></div>
+//           ))}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Grid;
+
 import React from 'react';
-import { range } from './utils';
+
+// Assuming you have a range function like this:
+// function range(start, end) {
+//   return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+// }
 
 function Grid({ numRows, numCols }) {
-  return (
-    <div className="grid">
-      {range(1, numRows).map((rowNum) => (
-        <div className="row" key={rowNum}>
-          {range(1, numCols).map((colNum) => (
-            <div className="cell" key={colNum}></div>
-          ))}
-        </div>
+  const rows = range(1, numRows).map((rowNum) => (
+    <div className="row" key={`row-${rowNum}`}>
+      {range(1, numCols).map((colNum) => (
+        <div className="cell" key={`cell-${rowNum}-${colNum}`}></div>
       ))}
     </div>
-  );
+  ));
+
+  return <div className="grid">{rows}</div>;
 }
 
 export default Grid;
+
